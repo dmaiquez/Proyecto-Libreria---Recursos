691c5b468e2629fbe0eb8c802344fa21c0231605
dmaiquez <dmaiquez@tecnologicoismac.edu.ec>
2026-05-31 23:01:53 -0500

    component cliente y otros archivos

---
+++ Configuraciones Iniciales/Recurso 4 - app-module.ts
@@ -0,0 +1,50 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { ClienteComponent } from './components/cliente/cliente';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+@NgModule({
+  declarations: [App
+    , ClienteComponent 
+  ],
+  imports: [
+      BrowserModule,
+      AppRoutingModule,          
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule,
+      FormsModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatButtonModule,
+      MatDialogModule,
+      MatSelectModule,
+      MatOptionModule,
+      MatDatepickerModule,
+      MatNativeDateModule
+
+
+  ],
+  providers: [provideBrowserGlobalErrorListeners(),
+         MatDatepickerModule  
+        ],
+  bootstrap: [App],
+})
+export class AppModule {}
